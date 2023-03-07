a8dffbb29c6fb90a50f4183aa5f1a1b0a7f81222
Daniel Kuhlwein <danielkuhlwein@me.com>
2023-03-06 17:56:00 -0700

    Upgrade to Angular v15.2.1

--- ClientApp/src/app/renters-insurance/renters-insurance.module.ts
+++ ClientApp/src/app/renters-insurance/renters-insurance.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RentersInsuranceComponent } from './renters-insurance.component';
+import { RentersRoutingModule } from 'src/app/renters-insurance/renters-insurance.routing.module';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [RentersInsuranceComponent]
+  imports: [CommonModule, RentersRoutingModule],
+  declarations: [RentersInsuranceComponent],
 })
-export class RentersInsuranceModule { }
+export class RentersInsuranceModule {}
--- ClientApp/src/app/renters-insurance/renters-insurance.routing.module.ts
+++ ClientApp/src/app/renters-insurance/renters-insurance.routing.module.ts
@@ -8,4 +8,4 @@ const routes: Routes = [{ path: '', component: RentersInsuranceComponent }];
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class FeatureRoutingModule {}
+export class RentersRoutingModule {}
