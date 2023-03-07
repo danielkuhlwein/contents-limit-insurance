a865b3eabe222c2b2d6212a8c1832f50d4b96ba6
Daniel Kuhlwein <danielkuhlwein@me.com>
2023-03-06 18:47:00 -0700

    Add Angular Material

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -4,7 +4,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from 'src/app/app.routing.module';
+import { MaterialModule } from 'src/app/material.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +15,8 @@ import { AppRoutingModule } from 'src/app/app.routing.module';
     HttpClientModule,
     FormsModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
